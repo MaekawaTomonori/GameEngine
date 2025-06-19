f55293d767cb283930ed91abd6aacf18a810445a
Apocalypse <k023g0063@g.neec.ac.jp>
2025-06-19 12:48:22 +0900

    Add Scene

--- include/IScene.hpp
+++ include/IScene.hpp
@@ -1,14 +1,31 @@
-﻿#ifndef IScene_HPP_
+#ifndef IScene_HPP_
 #define IScene_HPP_
+#include <memory>
+#include <string>
+
+#include "SceneSwitcher.hpp"
 
 
 class IScene {
+    SceneSwitcher* switcher_ = nullptr;;
+    bool progress_ = false;
+protected:
+    std::string next_;
+
+    // Fade Types
+    // in
+    // out
 public:
-    IScene();
-    ~IScene();
+    virtual ~IScene() = default;
+    virtual void Initialize() = 0;
+    virtual void Update() = 0;
+    virtual void Draw() = 0;
 
-private:
+    void IsProgress() const;
+    void Awake();
 
+protected:
+    void ChangeScene();
 }; // class IScene
 
 #endif // IScene_HPP_
---
+++ include/SceneSwitcher.hpp
@@ -0,0 +1,14 @@
+﻿#ifndef SceneSwitcher_HPP_
+#define SceneSwitcher_HPP_
+
+
+class SceneSwitcher {
+public:
+    SceneSwitcher();
+    ~SceneSwitcher();
+
+private:
+
+}; // class SceneSwitcher
+
+#endif // SceneSwitcher_HPP_
