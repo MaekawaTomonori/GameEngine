2486c3ae79c7383cd78f96d1975ce2cd448f0109
Apocalypse <k023g0063@g.neec.ac.jp>
2025-07-24 16:16:24 +0900

    Assetsがうまく同期されていなかったため更新

---
+++ Assets/Shaders/Vignette.PS.hlsl
@@ -0,0 +1,24 @@
+#include "CpyImg.hlsli"
+
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct Material {
+    float4 color;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+
+struct PixelOutput{
+    float4 color : SV_TARGET0;
+};
+
+PixelOutput main(VertexShaderOutput input) {
+    PixelOutput output;
+    output.color = gTexture.Sample(gSampler, input.texCoord);
+    float2 correct = input.texCoord * (1.f - input.texCoord.yx);
+    float value = correct.x * correct.y * 16.f;
+    value = saturate(pow(value, 0.8f));
+    output.color.rgb *= value;
+    output.color.a = 1.f;
+    return output;
+};
