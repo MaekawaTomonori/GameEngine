ef3c986ad66b27b9386836bd108c06c74fb51ce2
Apocalypse <k023g0063@g.neec.ac.jp>
2025-07-28 14:51:29 +0900

    BlendModeのセットが旧式のままだったので変更

--- src/PostProcess/Executor/PostProcessExecutor.cpp
+++ src/PostProcess/Executor/PostProcessExecutor.cpp
@@ -30,15 +30,6 @@ void PostProcessExecutor::Initialize(DirectXAdapter* _adapter, SRVManager* _srv,
         .RegisterSpace = 0,
         .OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND
     };
-    D3D12_BLEND_DESC blendDesc{};
-    blendDesc.RenderTarget[0].BlendEnable = true;
-    blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-    blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
-    blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
-    blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
-    blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
-    blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
-    blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
     
     //Create PSO
     pso_ = std::make_unique<PipelineStateObject>(adapter_);
@@ -62,7 +53,7 @@ void PostProcessExecutor::Initialize(DirectXAdapter* _adapter, SRVManager* _srv,
                 .ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL
             })
     )
-    .SetBlend(blendDesc)
+    .SetBlend(BlendMode::ALPHA)
     .SetShader(std::make_unique<Shader>(L"CpyImg"))
     .SetTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE)
     .Create();
