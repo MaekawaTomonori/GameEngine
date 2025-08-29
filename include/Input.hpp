e28846d7de6513a8931a3fc0d9cdbf59ace73abd
Apocalypse <k023g0063@g.neec.ac.jp>
2025-08-29 22:11:37 +0900

    IsPressを追加

--- include/Input.hpp
+++ include/Input.hpp
@@ -5,19 +5,23 @@
 #include <wrl/client.h>
 
 class Input {
+    HINSTANCE hInstance_{};
+    HWND hWnd_{};
+
     Microsoft::WRL::ComPtr<IDirectInput8> directInput_;
     Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard_;
     Microsoft::WRL::ComPtr<IDirectInputDevice8> mouse_;
     Microsoft::WRL::ComPtr<IDirectInputDevice8> joystick_;
-    BYTE keyState_[256];
-    BYTE preState_[256];
-    DIMOUSESTATE mouseState_;
-    DIMOUSESTATE preMouseState_;
+    BYTE keyState_[256]{};
+    BYTE preState_[256]{};
+    DIMOUSESTATE mouseState_{};
+    DIMOUSESTATE preMouseState_{};
 
     //EventSystem* eventSystem_ = nullptr;
 public:
-    void Initialize();
+    void Initialize(HWND _hWnd, HINSTANCE _hInstance);
     void Update();
+    bool IsPress(BYTE _key) const;
 
     //void SetEventSystem(EventSystem* eventSystem) { /*eventSystem_ = eventSystem;*/ }
 private:
--- src/Input/Input.cpp
+++ src/Input/Input.cpp
@@ -1,7 +1,50 @@
 #include "include/Input.hpp"
 
-void Input::Initialize() {
+#include "Log.hpp"
+#include "Utils.hpp"
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+void Input::Initialize(HWND _hWnd, HINSTANCE _hInstance) {
+    hWnd_ = _hWnd;
+    hInstance_ = _hInstance;
+
+    if (FAILED(DirectInput8Create(hInstance_, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput_, nullptr))) {
+        Log::Send(Log::Level::ERR, "Failed to create DirectInput8 instance.");
+        Utils::Alert("Failed to create DirectInput8 instance.");
+        return;
+    }
+
+    if (FAILED(directInput_->CreateDevice(GUID_SysKeyboard, &keyboard_, NULL))) {
+        Log::Send(Log::Level::ERR, "Failed to create keyboard device.");
+        Utils::Alert("Failed to create keyboard device.");
+        return;
+    }
+
+    if (FAILED(keyboard_->SetDataFormat(&c_dfDIKeyboard))) {
+        Log::Send(Log::Level::ERR, "Failed to set keyboard data format.");
+        Utils::Alert("Failed to set keyboard data format.");
+        return;
+    }
+
+    if (FAILED(keyboard_->SetCooperativeLevel(hWnd_, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY))) {
+        Log::Send(Log::Level::ERR, "Failed to set keyboard cooperative level.");
+        Utils::Alert("Failed to set keyboard cooperative level.");
+        return;
+    }
+
+
 }
 
 void Input::Update() {
+    if (keyboard_) {
+        keyboard_->Acquire();
+        memcpy_s(preState_, sizeof(keyState_), keyState_, sizeof(keyState_));
+        keyboard_->GetDeviceState(sizeof(keyState_), keyState_);
+    }
+}
+
+bool Input::IsPress(BYTE _key) const {
+    return keyState_[_key];
 }
