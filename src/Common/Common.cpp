721680dc046f1f8d5e8db1b00e0579ed48952ced
Apocalypse <k023g0063@g.neec.ac.jp>
2025-10-23 16:02:43 +0900

    描画処理の改善と新機能の追加
    `Renderer`クラスを使用するために`Common.cpp`と`Common.hpp`に`#include "src/Renderer/Renderer.hpp"`を追加しました。`Common::Draw`メソッドを更新し、`Renderer* _renderer`を引数として受け取るように変更しました。このメソッドでは、描画コマンドをポストエフェクトを適用するものとしないものに分けて処理します。
    
    `Common::RegisterCommand`メソッドを`Common::RegisterDebug`に名前変更し、新たに`Common::RegisterDraw`メソッドを追加しました。このメソッドは描画コマンドとポストエフェクトの適用フラグを受け取ります。
    
    `Common.hpp`に`RenderingCommand`構造体を追加し、描画コマンドとポストエフェクトの適用フラグを保持するようにしました。また、`Common`クラスに`std::vector<RenderingCommand> drawFunctions_`メンバ変数を追加し、描画コマンドを管理します。

--- src/Common/Common.cpp
+++ src/Common/Common.cpp
@@ -1,21 +1,52 @@
 #include "Common.hpp"
 
+#include "src/Renderer/Renderer.hpp"
+
 void Common::Setup(DirectXAdapter* _adapter, DebugUI* _debugUi) {
     std::lock_guard<std::mutex> lock(mutex_);
     adapter_ = _adapter;
     debugUI_ = _debugUi;
 }
 
-void Common::Draw() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (pipeline_){
-        pipeline_->DrawCall();
+void Common::Draw(Renderer* _renderer) {
+    std::vector<std::function<void()>> postEffectTasks;
+    std::vector<std::function<void()>> noPostEffectTasks;
+    {
+        for (const auto& command : drawFunctions_){
+            if (command.applyPostEffects){
+                postEffectTasks.push_back(command.func);
+            } else {
+                noPostEffectTasks.push_back(command.func);
+            }
+        }
+        drawFunctions_.clear();
     }
+
+    _renderer->Register([&]{
+        if (pipeline_){
+            pipeline_->DrawCall();
+        }
+
+        for (auto& task : noPostEffectTasks){
+            task();
+        }
+    });
+
+    _renderer->Register([&]{
+        for (auto& task : postEffectTasks){
+            task();
+        }
+    }, true);
+
 }
 
-void Common::RegisterCommand(const std::string &_id, const std::function<void()> &_command) {
+void Common::RegisterDebug(const std::string &_id, const std::function<void()> &_command) {
     std::lock_guard<std::mutex> lock(mutex_);
     if (debugUI_){
         debugUI_->RegisterCommand(_id, _command);
     }
+}
+
+void Common::RegisterDraw(const std::function<void()>& _command, bool _isApplyPostEffect) {
+    drawFunctions_.push_back({ _command, _isApplyPostEffect });
 }
